c42377fe7e89c8dc1fd93b69bbc361c148ec27dc
vilixal <vilixal@gmail.com>
2023-10-04 03:49:59 +0300

    МСХ (чисто расчеты без интеграций)

--- python_test/my_case/MSH.py
+++ python_test/my_case/MSH.py
@@ -1,19 +1,47 @@
 import datetime as dt
 
-date1 = dt.date(2022, 4, 10)
-date2 = dt.date(2022, 4, 10)
-date3 = dt.date(2023, 5, 1)
-date4 = dt.date(2022, 4, 10)
-date5 = dt.date(2022, 5, 1)
-plan = {date1: 360, date2: 270, date3: 2345, date4: 123, date5: -123}
+date1 = dt.date(2022, 8, 22)
+date2 = dt.date(2022, 10, 6)
+date3 = dt.date(2022, 10, 14)
+date4 = dt.date(2022, 10, 24)
+date5 = dt.date(2023, 4, 19)
+plan = {date1: 36000000, date2: 2700000, date3: 7140000, date4: 4375000, date5: -50215000}
 
-delta = date5 - date1
+date_s1 = dt.date(2022, 7, 10)
+date_s2 = dt.date(2022, 9, 19)
+stavka1, stavka2 = 8, 7.5
+stavka_list = {date_s1: stavka1, date_s2: stavka2}
+
+fatherly_year = 2022
+fatherly_month = 10
+
 date = date1
-date_list = [date]
-for i in range(delta.days):
-    date += dt.timedelta(1)
-    date_list.append(date)
-    print(date)
-    print(date_list)
 
-print(delta.days)
+subsidies_day, subsidies_month, subsidies, dolg, stavka, stavka_day, subsidies_year = 0, 0, 0, 0, 0, 0, 0
+while date <= date5:
+    subsidies_day, dolg, stavka_day = 0, 0, 1
+    # расчет долга за день
+    for date_operation, operation in plan.items():
+        if date_operation < date:
+            dolg += operation
+    # расчет ставки за день
+    for date_operation, stavka in stavka_list.items():
+        if date_operation < date:
+            stavka_day = stavka
+    # количество дней в году
+    if date.year % 4 != 0:
+        year = 365
+    else:
+        year = 366
+    subsidies_day = dolg * stavka_day / 100 / year
+    # субсидии за отчетный месяц
+    if date.year == fatherly_year and date.month == fatherly_month:
+        subsidies_month += subsidies_day
+    # субсидии за отчетный год
+    if (date.year == fatherly_year and date.month != 12) or (date.year == fatherly_year-1 and date.month == 12):
+        subsidies_year += subsidies_day
+    subsidies += subsidies_day
+    date += dt.timedelta(1)
+print('Субсидии за всё время', subsidies)
+print('Субсидии за отчетный год', subsidies_year)
+print('Субсидии за отчетный месяц', subsidies_month)
