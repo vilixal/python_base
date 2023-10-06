33e2827793b6384db014ec16be427af195ed5bcc
vilixal <vilixal@gmail.com>
2023-10-06 20:47:51 +0300

    7_snowfall

--- python_test/less7/7_snowfall.py
+++ python_test/less7/7_snowfall.py
@@ -10,46 +10,58 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
+def get_flakes(count=10,n=0):
+    flakes_list = []
+    for i in range(count):
+        flake = Snowflake(n)
+        flakes_list.append(flake)
+    return flakes_list
 
 class Snowflake:
 
-    def __init__(self):
-        self.x = 500
-        self.y = 500
+    def __init__(self,n=0):
+        self.x = randint(100,1100)
+        if n!=1:
+            self.y = randint(100,600)
+        else:
+            self.y = 600
         self.point = sd.get_point(self.x, self.y)
+        self.length =randint(20,40)
 
     def clear_previous_picture(self):
-        sd.snowflake(center=self.point, length=50, color=sd.background_color)
+        sd.snowflake(center=self.point, length=self.length, color=sd.background_color)
 
 
     def move(self):
-        self.y -= 10
+        self.y -= 5+(self.length-20)/5
         self.x += randint(-10,10)
 
     def draw(self):
         self.point = sd.get_point(self.x, self.y)
-        sd.snowflake(center=self.point, length=50, color=sd.COLOR_WHITE)
+        sd.snowflake(center=self.point, length=self.length, color=sd.COLOR_WHITE)
 
-def get_flakes(count=10):
-    flakes_list = []
-    for i in range(count):
-        flake = Snowflake()
-        flakes_list.append(flake)
-    return flakes_list
+    def __bool__(self):
+        return self.y<=self.length
 
 
-
-
-flake = Snowflake()
-# flakes = get_flakes(count=N)
+# flake = Snowflake()
+count=20
+flakes = get_flakes(count=count)
 
 while True:
+    flakes_delta = []
+    i=count-1
+    for flake in flakes[count-1:0:-1]:
+        if flake:
+            flakes.pop(i)
+        i-=1
+    flakes_delta = get_flakes(count=count-len(flakes),n=1)
+    flakes.extend(flakes_delta)
     sd.start_drawing()
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    # if not flake.can_fall():
-    #     break
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
     sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
