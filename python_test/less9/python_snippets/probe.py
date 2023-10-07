43efef685ca9c6621090ce8c472bbb0342722e73
vilixal <vilixal@gmail.com>
2023-10-07 21:38:36 +0300

    добавил 9 лекцию

---
+++ python_test/less9/python_snippets/probe.py
@@ -0,0 +1,11 @@
+file_name = 'pushkin.txt'
+file = open(file_name, mode='r', encoding='utf8')
+line = True
+while line:
+    line = file.readline()
+    if 'красавица' in line:
+        print('Красавица найдена в строке', line)
+        break
+else:
+    print('Тут красавиц нет')
+file.close()
