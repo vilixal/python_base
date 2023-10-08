395306fda29e25b43bcd775023222f9a007204a9
vilixal <vilixal@gmail.com>
2023-10-08 22:57:09 +0300

    добавил урок 10 + первый пример

---
+++ python_test/less10/01_fifth_element.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
+
+BRUCE_WILLIS = 42
+
+input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except IndexError as ext:
+    print(f'{ext}:\n{ext.args}: Слишком короткая строка: "{input_data}"')
+except ValueError as ext:
+    print(f'{ext}:\n{ext.args}: Некорректный в строке: "{input_data}"')
+except Exception as ext:
+    print("Неизвестная ошибка.")
+
+
+
+
+# Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
+# - ValueError - невозможно преобразовать к числу
+# - IndexError - выход за границы списка
+# - остальные исключения
+# для каждого типа исключений написать на консоль соотв. сообщение
+
+
+
+
