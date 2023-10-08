0eef09bff8364160bb00872d07b43eeaf3f2a2ff
vilixal <vilixal@gmail.com>
2023-10-08 13:21:51 +0300

    сортировки словаря

--- python_test/less9/01_char_stat.py
+++ python_test/less9/01_char_stat.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
+
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -21,7 +23,39 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+def print_stat(stat_sorted):
+    global char, count
+    print('+{txt:-^9}+{txt:-^9}+'.format(txt='-'))
+    print('|{txt:^9}|{txt2:^9}|'.format(txt='буква', txt2='частота'))
+    print('+{txt:-^9}+{txt:-^9}+'.format(txt='-'))
+    for char, count in stat_sorted:
+        print(f'|{char:^9}|{count:^9}|')
+    print('|{txt:-^9}|{txt:-^9}|'.format(txt='+'))
+
+
+stat = {}
+with open('python_snippets\\voyna-i-mir.txt', 'r', encoding='cp1251') as file:
+    for line in file:
+        for char in line:
+            if char.isalpha():
+                if char in stat:
+                    stat[char] += 1
+                else:
+                    stat[char] = 1
+# pprint(stat)
+
+
+# stat_sorted_key = sorted(stat.items())
+# print_stat(stat_sorted_key)
+# stat_sorted_revers = sorted(stat.items(), reverse=True)
+# print_stat(stat_sorted_revers)
+# stat_sorted_value = sorted(stat.items(), key=lambda item: item[1]) # https://pythonru.com/osnovy/vozmozhnosti-i-primery-funkcii-sorted-v-python
+# print_stat(stat_sorted_value)
+
+def sort_key(e):
+    return e[1]
+
+print_stat(sorted(stat.items(),key=sort_key))
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
