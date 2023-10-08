06fe16ea3adc1fe3b768ab33fcc4fca44bbaf0da
vilixal <vilixal@gmail.com>
2023-10-08 23:36:11 +0300

    свои исключения + парсинг email

--- python_test/less10/03_registration_log.py
+++ python_test/less10/03_registration_log.py
@@ -22,5 +22,39 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-with open('registrations.txt','r',encoding='utf8'):
+import re
+
+class NotNameError(Exception):
     pass
+
+class NotEmailError(Exception):
+    pass
+
+def check(line):
+    name,email,age=line.split(' ')
+    if name.isalpha() is not True:
+        raise NotNameError
+    pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
+    # pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    if re.match(pattern,email) is None:
+        raise NotEmailError
+    if 10 >= int(age) >= 99 is not True:
+        raise ValueError('Неверный возраст')
+
+
+with open('registrations.txt','r',encoding='utf8') as file:
+    count=0
+    for line in file:
+        line=line.strip()
+        count+=1
+        try:
+            check(line)
+        except NotNameError as ext:
+            print(f'{count}: В строке "{line}" возникло исключение NotNameError: "Имя содержит посторонние символы"')
+        except NotEmailError as ext:
+            print(f'{count}: В строке "{line}" возникло исключение NotEmailError: "Некорретный email"')
+        except ValueError as ext:
+            if 'unpack' in ext.args[0]:
+                print(f'{count}: В строке "{line}" возникло исключение ValueError:{ext}')
+            else:
+                print(f'{count}: В строке "{line}" возникло исключение ValueError: "Некорретный возвраст"')
--- python_test/test.py
+++ python_test/test.py
@@ -1,7 +1,8 @@
-line = '[2018-05-17 01:57:16.665804] NOK'
-date, time, res = line.split(' ')
-dt = date + ' ' + time[:5] + time[-1:]
-print(date)
-print(time)
-print(res)
-print(dt)
+import re
+
+email = '@1.2'
+pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+if re.match(pattern,email):
+    print('correct')
+else:
+    print('NOT')
