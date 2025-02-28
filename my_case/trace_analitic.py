87e5b5e673fe0fcf88d91e7745e3fbb96d9c849c
1 <1>
2025-02-28 18:01:13 +0300

    con

--- my_case/trace_analitic.py
+++ my_case/trace_analitic.py
@@ -72,13 +72,18 @@ Table                              Natural     Index    Update    Insert    Dele
 **********************************************************************************
 DOCSTATUS                             1372                                              '''
 
-# dict timestamp,process_id,thread_id,thread_number,event,db,attachment,user,process_name,transaction,sql_text,time_execute
-timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text, time_execute = (0,) * 12
-first_line = True
+timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text, time_execute = (
+                                                                                                                                      0,) * 12
+
+def new_line():
+    global timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text, time_execute
+    timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text, time_execute = (
+                                                                                                                                          0,) * 12
+
+dict_trace={}
 first_line_sql = False
 i=0
 lines = file.split('\n')
-trace
 for line in lines:
     match_timestamp = re.match(r'(?P<timestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{4})\s\((?P<process_id>\d+):(?P<thread_id>\w+)#(?P<thread_number>\d+)\)\s(?P<event>\w+)',line)
     if match_timestamp:
@@ -87,7 +92,7 @@ for line in lines:
         thread_id = match_timestamp.group("thread_id")
         thread_number = match_timestamp.group("thread_number")
         event = match_timestamp.group("event")
-        first_line = False
+        i += 1
         continue
     match_att=re.search(r'(?P<db>[-a-zA-Z0-9_]+)\s\(ATT_(?P<attachment>\d+),\s(?P<user>[-a-zA-Z0-9_]+):\w+,\s\w+,\s',line)
     if match_att:
@@ -98,24 +103,30 @@ for line in lines:
     match_proc=re.search(r'(?P<process_name>NCORE:[\w\s\[\]()-]+):\d+$',line)
     if match_proc:
         process_name=match_proc.group('process_name')
+        continue
     match_transaction = re.search(r'\(TRA_(?P<transaction>\d+),\s\w+\s\|\s\w+\s\|\s\w+\s\d+\s\|\s\w+\)$', line)
     if match_transaction:
         transaction=match_transaction.group('transaction')
+        continue
     match_sql=re.search(r'[-]{79}',line)
     match_end_sql=re.search(r'[\^]{79}',line)
     if match_end_sql:
         first_line_sql=False
+        continue
     if first_line_sql:
         sql_text+=line
+        continue
     if match_sql:
         first_line_sql=True
         sql_text=''
         match_sql=None
+        continue
     match_execute=re.search(r'(?P<time_execute>\d+)\sms,\s\d+\s',line)
     if match_execute:
         time_execute=match_execute.group('time_execute')
-        i+=1
-        first_line = True
+        dict_trace[i]=[timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text,
+      time_execute]
+        new_line()
 
 
 print(timestamp, process_id, thread_id, thread_number, event, db, attachment, user, process_name, transaction, sql_text,
